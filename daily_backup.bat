c6ff1d999ec469c56e730db5b66f2a3214358f5e
NIsarg Shah <33025303+nisarg05@users.noreply.github.com>
2018-11-13 12:05:56 +0530

    Update daily_backup.bat

--- daily_backup.bat
+++ daily_backup.bat
@@ -2,4 +2,4 @@
 set day=%date:~0,2%
 set month=%date:~3,2%
 set year=%date:~6,4%
-robocopy "\\3dbp-india\fileserver\1000_NL_Project\1002_Rochdale_RunningProj" "F:\daily\RD_%year%%month%%day%" /E /maxlad:%year%%month%%day% /a+:R /njh /log:"F:\log\RD_%year%%month%%day%.log"
+robocopy "backup_directory" "Destination_directory_%year%%month%%day%" /E /maxlad:%year%%month%%day% /a+:R /njh /log:"Log_Directory_%year%%month%%day%.log"
